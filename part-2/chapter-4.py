dd1465161116838a8f967ac9fa7cc56eb115e231
Jim Doran <jimdoran26@gmail.com>
2020-12-31 13:31:46 -0500

    string commands

---
+++ part-2/chapter-4.py
@@ -0,0 +1,30 @@
+#integer addition
+print(123+222)
+#floating point
+print(1.5*4)
+#exponent
+print(2**100)
+
+import math
+print(math.pi)
+print(math.sqrt(85))
+
+s='spam'
+x=len(s)
+print(x)
+print(s[0])
+print(s[-1])
+
+print(s.find('pa'))
+print(s.find('ap'))
+
+print(s.replace('pa','xyz'))
+print(s)
+
+
+line = 'aaa,bbb,ccccccc,dd'
+print(line.split(','))
+
+print(s.isalpha())
+
+print(line.rstrip().split(','))
