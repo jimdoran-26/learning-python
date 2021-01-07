3d853b746e75507cb31ce724d731d32b2206e7de
Jim Doran <jimdoran26@gmail.com>
2021-01-07 16:37:39 -0500

    chapter 10/11

---
+++ part-3/chapter-10-11.py
@@ -0,0 +1,18 @@
+a,b='good','bad'
+print(a,b)
+
+if 'python' in 'text':
+    print('python')
+
+def gen(n):
+    for i in n: yield i*2
+
+
+
+a, b, c, d = 'spam'
+print(a,b,c,d)
+
+a, *b = 'spam'
+print(a,*b)
+
+x=2
